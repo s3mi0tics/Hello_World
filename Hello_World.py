192ef543a32652586537f856838499a7f1bdfe4e
Colby Kauk <colby.kauk@gmail.com>
2021-11-29 15:42:33 -0700

    change name, convert number to string so it concatinates

--- Hello_World.py
+++ Hello_World.py
@@ -1,13 +1,13 @@
 # 1. TASK: print "Hello World"
 print("Hello World")
 # 2. print "Hello Noelle!" with the name in a variable
-name = "Noelle"
+name = "Bunsen"
 print("Hello", name)	# with a comma
 print("Hello " + name)	# with a +
 # 3. print "Hello 42!" with the number in a variable
-name = 42
+name = 55
 print("Hello", name)	# with a comma
-print("Hello" + name)	# with a +	-- this one should give us an error!
+print("Hello " + str(name))	# with a +	-- this one should give us an error!
 # 4. print "I love to eat sushi and pizza." with the foods in variables
 fave_food1 = "sushi"
 fave_food2 = "pizza"
